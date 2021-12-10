f9fb7058bac4985f7c550ef06d82b97b14f57918
Tomas Steikunas <tsteikunas@gmail.com>
2021-12-10 11:39:29 +0200

    Puslapiu dalinimas

---
+++ IT ATEITIS/ITlyga.js
@@ -0,0 +1,7 @@
+$ (function () {
+    $('header').load('/HTML/header.html');
+    $('aside').load('/html/aside.html');
+    $('footer').load('HTML/footer.html');
+
+    $('main').load('/main.html');
+});
